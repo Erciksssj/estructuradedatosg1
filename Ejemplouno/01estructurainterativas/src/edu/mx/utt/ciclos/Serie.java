b7dcb4fe36c3ad064c27670077d127526d69e51f
Erick Trejo Resendiz <eri05.ff08@gmail.com>
2024-09-05 12:58:16 -0600

    Logro:Se realizo practica en visual studio code creando la clase serie con los metodos for y while

---
+++ Ejemplouno/01estructurainterativas/src/edu/mx/utt/ciclos/Serie.java
@@ -0,0 +1,56 @@
+package edu.mx.utt.ciclos;
+
+public class Serie {
+
+    // variable de instancia
+    private int n;
+
+    // contructor
+    public Serie() {
+        this.n = 1;
+    }
+
+    public Serie(int n) {
+        // if (n>0) {
+        // this.n=n;
+        // } else {
+        // this.n=1;
+        // }
+        this.n = (n <= 0) ? 1 : n;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = (n>0)?n:1;
+    }
+
+    public double calcularFor(){
+        double r = 0.0;
+        double constante=1.0;
+        for (int i = 1; i <= this.n; i++) {
+            r+=constante/i;
+        }
+        return 0.0;
+    }
+
+    public double calcularWhilw(){
+        double r = 0.0;
+        double constante=1.0;
+        double i=1;
+        while (i <= this.n) {
+            r+=constante/i;
+            i++;
+        }
+        return 0.0;
+    }
+
+    @Override
+    public String toString() {
+        return "Serie [n=" + n + "]";
+    }
+    
+    
+}
---
+++ Ejemplouno/01estructurainterativas/src/edu/mx/utt/ciclos/TestSerie.java
@@ -0,0 +1,5 @@
+package edu.mx.utt.ciclos;
+
+public class TestSerie {
+
+}
